e596974aeb8ae139db6f99a792d33f76fbe71048
smovenden <steven.o@retailexpress.com.au>
2021-06-23 15:21:00 +1000

    Hi All!

---
+++ CSharpBasicTraining/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSharpBasicTraining
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //output to screen
+            Console.WriteLine("World end in..." + Sum(10, 12));
+        }
+        
+        public static int Sum(int var1, int var2)
+        {
+            //calculation sum returned from call
+            return var1 + var2;
+        }
+    }
+}
