fb760ed6da7c0fef0a965fa3f3f7fe4a730d3658
smovenden <steven.o@retailexpress.com.au>
2021-06-23 16:24:19 +1000

    If-case statements

---
+++ CSharpBasicTraining/0_FirstSteps/BasicsCSharp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBasicTraining._0_FirstSteps
+{
+    public class BasicsCSharp
+    {
+        public static void BasicsCSharpRun()
+        {
+            //Variable declaration
+            string message;
+
+            //Variable assignment
+            message = "have a nice day!";
+
+            //Outut ariable
+            Console.WriteLine("Hi Retail Express - " + message);
+
+            //output to screen
+            Console.WriteLine("World end in..." + Sum(10, 12));
+        }
+
+        public static int Sum(int var1, int var2)
+        {
+            //calculation sum returned from call
+            return var1 + var2;
+        }
+    }
+}
